a556ea7ba22ab7703fc04473db9a2b00eb6005aa
MartsLine <48711841+MartsLine@users.noreply.github.com>
2020-01-22 16:17:41 +0400

    Update Animal.java

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,21 @@
 package com.company;
 
 public class Animal {
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public Animal() {
+    }
 }
